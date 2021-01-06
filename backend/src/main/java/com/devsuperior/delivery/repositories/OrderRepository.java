548e39b96ff25421d256f31b385cc2bdde621511
unknown <rikegg16@gmail.com>
2021-01-06 20:23:19 -0300

    Read to prod

--- backend/src/main/java/com/devsuperior/delivery/repositories/OrderRepository.java
+++ backend/src/main/java/com/devsuperior/delivery/repositories/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import com.devsuperior.delivery.entities.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-	//teste
+	
 	@Query("SELECT DISTINCT obj FROM Order obj JOIN FETCH obj.products"
 			+ " WHERE obj.status = 0 ORDER BY obj.moment ASC")
 	List<Order> findOrderWithProducts();
